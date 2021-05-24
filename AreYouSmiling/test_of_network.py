8777015c33dd8150f933f84ab408f4f1c71888ba
OlhaBabicheva <57872493+OlhaBabicheva@users.noreply.github.com>
2021-05-24 17:16:12 +0200

    Add files via upload

---
+++ AreYouSmiling/test_of_network.py
@@ -0,0 +1,7 @@
+import pickle
+import tensorflow as tf
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
